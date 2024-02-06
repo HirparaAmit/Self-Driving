475f52042acda647865729a328924b3df4c2b5b0
Amit Hirpara <hirparaamit43@gmail.com>
2024-02-06 20:26:23 +0530

    Control Vehicle

---
+++ api/control_robot.py
@@ -0,0 +1,19 @@
+import sim
+import time, sys
+
+print("Program Started")
+sim.simxFinish(-1)
+clientID = sim.simxStart('127.0.0.1', 19999, True, True, 5000, 5)
+
+if(clientID != -1):
+    print('Connected Successfully')
+else:
+    sys.exit('Failed To Connect')
+
+time.sleep(1)
+
+error_code, left_motor_handle = sim.simxGetObjectHandle(clientID, '/PioneerP3DX/leftMotor', sim.simx_opmode_oneshot_wait)
+error_code, right_motor_handle = sim.simxGetObjectHandle(clientID, '/PioneerP3DX/rightMotor', sim.simx_opmode_oneshot_wait)
+
+error_code = sim.simxSetJointTargetVelocity(clientID, left_motor_handle, 8, sim.simx_opmode_oneshot_wait)
+error_code = sim.simxSetJointTargetVelocity(clientID, right_motor_handle, 6, sim.simx_opmode_oneshot_wait)
